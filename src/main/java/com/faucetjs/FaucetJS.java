9240bb968ce308acaba6df16b5de222b77ffae9e
Aerh <callum.aero@gmail.com>
2017-10-22 15:28:21 +0100

    Creation of 'scripts' directory

--- src/main/java/com/faucetjs/FaucetJS.java
+++ src/main/java/com/faucetjs/FaucetJS.java
@@ -2,15 +2,29 @@ package com.faucetjs;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.util.logging.Level;
+
 public class FaucetJS extends JavaPlugin {
+    private String scriptDirectory = null;
 
     @Override
     public void onEnable() {
+        //saveConfig();
+        scriptDirectory = getConfig().getString("settings.scripts-folder");
+        File directory = new File(getDataFolder(), scriptDirectory == null ? "scripts" : scriptDirectory);
 
+        if (directory.mkdirs()) {
+            getLogger().log(Level.INFO, "Successfully created the \'" + directory.getName() + "\' directory!");
+        }
     }
 
     @Override
     public void onDisable() {
+        saveConfig();
+    }
 
+    public String getScriptDirectory() {
+        return scriptDirectory;
     }
 }
