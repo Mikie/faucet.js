2736326faee81bf9af49b532f7b5e9e5482f0cd6
Aerh <callum.aero@gmail.com>
2017-10-22 14:07:40 +0100

    Start of initial plugin

---
+++ src/main/java/com/faucetjs/FaucetJS.java
@@ -0,0 +1,16 @@
+package com.faucetjs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FaucetJS extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
